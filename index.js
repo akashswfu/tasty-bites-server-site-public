8a4d41ac5c1da5a73cfbbe052a5e214a0bb5b79a
akashswfu <splashakash99@gmail.com>
2024-05-12 12:29:32 +0600

    token recive on client site

--- index.js
+++ index.js
@@ -39,6 +39,23 @@ async function run() {
     const foodsCollection = client.db('foodsDB').collection('foodItem');
     const foodsRequestCollection = client.db('foodsDB').collection('foodReq');
 
+    // jwt generator
+    app.post('/jwt',async(req,res)=>{
+      const user = req.body;
+      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn:'365d',       
+      })
+      res.cookie('token',token,{
+        httpOnly:true,
+        secure:process.env.NODE_ENV === 'production',
+        sameSite:process.env.NODE_ENV === 'production' ? 'none':'strict'
+
+      }).send({success:true})
+    })
+
+
+
+
     
 
     // get all available food data
